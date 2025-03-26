63f4a40d7e6165120dd4360b09de69aa1f81f561
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-26 17:27:28 +0100

    [FIX] Fix javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+const form = document.querySelector("form");
+const kmInput = document.getElementById("km");
+const ageInput = document.getElementById("seleziona-etá");
+const pulsanteBtn = document.getElementById("genera");
+const kmPercorsiElement = document.getElementById("km-percorsi");
+const ticketPriceElement= document.getElementById("prezzo-biglietto");
 
-document.getElementById("genera").addEventListener("click", function () {
+document.getElementById("genera").addEventListener( "click", function () {
 
     const km = document.getElementById("km").value;
     const age = document.getElementById("seleziona-etá").value;
@@ -13,23 +19,24 @@ document.getElementById("genera").addEventListener("click", function () {
     else if (age >= 65) {
         ticketPrice *= 0.6
     }
+
     ticketPrice = ticketPrice.toFixed(2);
+
+    kmPercorsiElement.textContent = km + " km";
+    
+
     console.log(`Il prezzo del biglietto é : ${ticketPrice} €`)
 
 });
 
 
-const form = document.querySelector("form");
-const km = document.getElementById("km");
-const age = document.getElementById("seleziona-etá");
-const pulsante = document.getElementById("genera");
-const kmpercorsi = document.getElementById("km-percorsi");
-const prezzobiglietto = document.getElementById("prezzo-biglietto");
 
-form.addEventListener('submit', function(event){
+
+form.addEventListener("submit", function(event){
     //evita il refresh pagina
     event.preventDefault();
+    generaBtn.click();
 
-    kmpercorsi.innerHTML = km.value
-    prezzobiglietto.innerHTML = ticketPrice.value
+    
 });
+
