c8b807cbed9a55c99fdf1100650a0e81ec886a21
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-27 00:55:19 +0100

    [DONE] Add new content and fix

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ const pulsanteGenera = document.getElementById("genera");
 const pulsanteAnnulla = document.getElementById("annulla");
 const kmPercorsiElement = document.getElementById("km-percorsi");
 const ticketPriceElement= document.getElementById("prezzo-biglietto");
-
+const nomeUtenteElement = document.getElementById("nome-cognome");
+const nomeInput = document.getElementById("nome-cognome-output");
 document.getElementById("genera").addEventListener( "click", function () {
     
     const km = document.getElementById("km").value;
     const age = document.getElementById("seleziona-etá").value;
+    const nomeUtente = nomeInput.value;
 
     let ticketPrice = km * 0.21;
 
@@ -25,14 +27,16 @@ document.getElementById("genera").addEventListener( "click", function () {
 
     kmPercorsiElement.textContent = km + " km";
     ticketPriceElement.textContent = ticketPrice + " €";
+    nomeUtenteElement.textContent = nomeUtente;
 
     console.log(`Il prezzo del biglietto é : ${ticketPrice} €`)
 
 });
 
 document.getElementById("annulla").addEventListener( "click", function () {
-    document.getElementById("km").value ="" ;
-    document.getElementById("seleziona-etá").value ="" ;
+    document.getElementById("km").value =" " ;
+    document.getElementById("seleziona-etá").value =" " ;
+    document.getElementById("nome-cognome-output").value=" ";
 
 
 });
