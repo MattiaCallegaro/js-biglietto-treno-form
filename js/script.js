a6382f0e57e62a39244106fa44f86e886d9b34db
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-27 10:42:49 +0100

    [FIX] correction fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//recupero elementi DOM
 const form = document.querySelector("form");
 const kmInput = document.getElementById("km");
 const ageInput = document.getElementById("seleziona-etá");
@@ -7,14 +8,17 @@ const kmPercorsiElement = document.getElementById("km-percorsi");
 const ticketPriceElement= document.getElementById("prezzo-biglietto");
 const nomeUtenteElement = document.getElementById("nome-cognome");
 const nomeInput = document.getElementById("nome-cognome-output");
+
 document.getElementById("genera").addEventListener( "click", function () {
-    
+    //valori recuperati dal form
     const km = document.getElementById("km").value;
     const age = document.getElementById("seleziona-etá").value;
     const nomeUtente = nomeInput.value;
 
+    //definisco la costante moltiplicativa
     let ticketPrice = km * 0.21;
 
+    //definisco i prezzi in base all'età
     if (age < 18) {
         ticketPrice *= 0.8;
     }
@@ -22,21 +26,20 @@ document.getElementById("genera").addEventListener( "click", function () {
     else if (age >= 65) {
         ticketPrice *= 0.6
     }
-
-    ticketPrice = ticketPrice.toFixed(2);
-
-    kmPercorsiElement.textContent = km + " km";
-    ticketPriceElement.textContent = ticketPrice + " €";
-    nomeUtenteElement.textContent = nomeUtente;
-
-    console.log(`Il prezzo del biglietto é : ${ticketPrice} €`)
+   
+    //permetto di visualizzzare il testo javascript a schermo
+    ticketPriceElement.innerHTML =`<strong>${ticketPrice.toFixed(2)}</strong>` ;
+    nomeUtenteElement.innerText = nomeUtente;
+    kmPercorsiElement.innerText = km;
 
 });
+    //creo la funzione annulla per cancellare tutti i valori inseriti.
+    document.getElementById("annulla").addEventListener( "click", function () {
 
-document.getElementById("annulla").addEventListener( "click", function () {
     document.getElementById("km").value =" " ;
     document.getElementById("seleziona-etá").value =" " ;
     document.getElementById("nome-cognome-output").value=" ";
+    
 
 
 });
